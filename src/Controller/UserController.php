ffb660d31f4eb3b595f53d66c3f8610e1a195c9c
roama <rowan.amatkario@eonics.nl>
2021-11-18 15:06:45 +0100

    add login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/homepage", name="homepage")
      */
     public function index(): Response
     {
@@ -23,7 +23,27 @@ class UserController extends AbstractController
      */
     public function bestellingen()
     {
-        return $this->render('homepage/index.html.twig', [
+        return $this->render('bestellingen/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/menu", name="menu")
+     */
+    public function menu()
+    {
+        return $this->render('menu/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/vestigingen", name="vestigingen")
+     */
+    public function vestigingen()
+    {
+        return $this->render('vestigingen/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
