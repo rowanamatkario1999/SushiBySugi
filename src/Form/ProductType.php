6daafa6cb1cdafc8cc85fa6b64c133321c90be6d
roama <rowan.amatkario@eonics.nl>
2021-11-25 17:40:37 +0100

    fix the choiceType from entity

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use SebastianBergmann\CodeCoverage\Report\Text;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,11 +28,11 @@ class ProductType extends AbstractType
             ->add('description', TextareaType::class,[
                 'label' => 'Beschrijving :'
             ])
-            ->add('categoryId', EntityType::class,[
+            ->add('categoryId', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
                 'label' => 'Categorie :'
-            ])
+            ]);
         ;
     }
 
