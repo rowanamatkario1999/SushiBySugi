58dbbb4faafece133d0968d2c330f9098756dcea
roama <rowan.amatkario@eonics.nl>
2021-11-25 18:31:22 +0100

    finish the user experience

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/product")
+ * @Route("/product)
  */
 class ProductController extends AbstractController
 {
